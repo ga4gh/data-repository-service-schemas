dc7550b888aa8ba19a3c65cb93c02a3985d7289e
David Steinberg <david@resium.com>
2017-10-27 14:42:26 -0700

    Change URL to include metadata @bwalsh points out that file metadata is unique to the blob store and URL instance.

--- demo.py
+++ demo.py
@@ -8,11 +8,12 @@ def client_demo():
     # CreateDataObject
     print("..........Create an object............")
     Checksum = models.get_model('ga4ghChecksum')
+    URL = models.get_model('ga4ghURL')
     CreateDataObjectRequest = models.get_model('ga4ghCreateDataObjectRequest')
     create_request = CreateDataObjectRequest(
         file_name="abc",
         checksum=[Checksum(checksum="def", type=0)],
-        urls=["a", "b"])
+        urls=[URL(url="a"), URL(url="b")])
     create_response = client.CreateDataObject(body=create_request).result()
     data_object_id = create_response['data_object_id']
     print(data_object_id)
@@ -28,7 +29,7 @@ def client_demo():
     update_request = UpdateDataObjectRequest(
         file_name="abc",
         checksum=[Checksum(checksum="def", type=0)],
-        urls=["a", "b", "c"])
+        urls=[URL(url="a"), URL(url="b"), URL(url="c")])
     update_response = client.UpdateDataObject(
         data_object_id=data_object_id, body=update_request).result()
     updated_object = client.GetDataObject(
