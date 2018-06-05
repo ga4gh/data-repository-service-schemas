3869100fff26d43a3af47fd5ea77ac091ce6e3fd
Natan Lao <natanlao@users.noreply.github.com>
2018-06-05 14:16:30 -0700

    Test version across setup.py, OpenAPI

---
+++ python/test/test_package.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+import unittest
+
+import yaml
+
+class TestPackage(unittest.TestCase):
+    @classmethod
+    def get_spec_version(cls, filename):
+        with open(filename, 'r') as f:
+            spec = yaml.safe_load(f)
+        return spec['info']['version']
+
+    def test_version_consensus(self):
+        from ga4gh.dos import __version__
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        spec_dir = os.path.join(cwd, "../../openapi")
+        for f in os.listdir(spec_dir):
+            assert __version__ == self.get_spec_version(os.path.join(spec_dir, f))
