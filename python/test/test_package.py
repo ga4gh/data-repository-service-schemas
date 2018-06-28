46d9caa03807b9d826c1c69202caaab24a2d9e7c
Natan Lao <natanlao@users.noreply.github.com>
2018-06-27 19:44:28 -0700

    Test Swagger schema validity (closes #100) (#101)
    * Test Swagger schema validity (closes #100)
    
    * Test schema validity with swagger-spec-validator
    
    The version installed by default has a bug that causes the
    schema validation test to fail, so we need to manually specify
    swagger-spec-validator==2.3.1 (or greater) for validation.

--- python/test/test_package.py
+++ python/test/test_package.py
@@ -2,10 +2,16 @@
 import os
 import unittest
 
+import swagger_spec_validator
 import yaml
 
 
 class TestPackage(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        cls.spec_dir = os.path.join(cwd, "../../openapi")
+
     @classmethod
     def get_spec_version(cls, filename):
         with open(filename, 'r') as f:
@@ -14,7 +20,14 @@ class TestPackage(unittest.TestCase):
 
     def test_version_consensus(self):
         from ga4gh.dos import __version__
-        cwd = os.path.dirname(os.path.realpath(__file__))
-        spec_dir = os.path.join(cwd, "../../openapi")
-        for f in os.listdir(spec_dir):
-            assert __version__ == self.get_spec_version(os.path.join(spec_dir, f))
+        for f in os.listdir(self.spec_dir):
+            assert __version__ == self.get_spec_version(os.path.join(self.spec_dir, f))
+
+    def test_schema_validity(self):
+        """Check to make sure that the Swagger schema specification is
+        valid by running it through the online validator. If the schema
+        is valid, the online validator will return 200 OK and an empty
+        JSON response."""
+        path = os.path.join(self.spec_dir, 'data_object_service.swagger.yaml')
+        swagger_spec_validator.validate_spec_url('file://' + os.path.abspath(path))
+
