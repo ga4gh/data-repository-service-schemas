24ba9b3c32c5a4b449db90c8293b09f8bfc849dc
Natan Lao <natanlao@users.noreply.github.com>
2018-08-28 15:18:29 -0700

    Add mechanism to skip certain compliance tests
    This is useful when running the compliance test
    suite against an implementation that has not
    achieved full schema parity with DOS.

--- python/ga4gh/dos/test/compliance.py
+++ python/ga4gh/dos/test/compliance.py
@@ -1,17 +1,58 @@
 # -*- coding: utf-8 -*-
+import functools
 import json
 import logging
 import time
 import unittest
-import uuid
-
 try:
     import urllib.parse as urllib  # For Python 3 compat
 except ImportError:
     import urllib
+import uuid
+
+import ga4gh.dos.schema
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+def test_requires(*operations):
+    """
+    This is a decorator that identifies what DOS operations a given test
+    case uses (where each DOS operation is named by its `operationId` in
+    the schema, e.g. ListDataBundles, UpdateDataObject, GetServiceInfo,
+    etc.) and skips them if the operation is not supported by the
+    implementation under test.
+
+    For example, given this test setup::
+
+        class Test(AbstractComplianceTest):
+            supports = ['UpdateDataBundles']
+
+            @test_requires('UpdateDataBundles')
+            def test_update_data_bundles(self):
+                self.dos_request('PUT', '/databundles/1234')
+
+            @test_requires('ListDataBundles', 'UpdateDataBundles')
+            def test_list_and_update_data_bundles(self):
+                self.dos_request('GET', '/databundles')
+                self.dos_request('PUT', '/databundles/1234')
+
+    ``test_update_data_bundles`` would run and ``test_list_and_update_data_bundles``
+    would be skipped.
+
+    :param str \*operations: the operations supported by the decorated
+                             test case
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(self):
+            unsupported = [op for op in operations if op not in self.supports]
+            if unsupported:
+                raise unittest.SkipTest("not supported: " + ", ".join(unsupported))
+            return func(self)
+        return wrapper
+    return decorator
 
 
 class AbstractComplianceTest(unittest.TestCase):
@@ -52,7 +93,30 @@ class AbstractComplianceTest(unittest.TestCase):
 
     You would then be able to run the compliance test suite however you
     normally run your tests (e.g. ``nosetests`` or ``python -m unittest discover``).
+
+    :var supports: a list of supported DOS implementations. By default, this is
+                   the list of all DOS operations, named by the `operationId`
+                   key in the schema::
+
+                       supports = ['GetServiceInfo', 'GetDataBundleVersions',
+                                   'CreateDataBundle', 'ListDataBundles',
+                                   'UpdateDataObject', 'GetDataObject', ...]
+
+                    Adding / removing operations from this list will adjust
+                    which tests are run. So, doing something like::
+
+                        class Test(AbstractComplianceTest):
+                            self.supports = ['ListDataObjects']
+
+                    would skip all tests calling UpdateDataBundle, GetDataBundle,
+                    and any other endpoint that is not ListDataObjects.
     """
+    # Populate :var:`supports` with the `operationId` of each DOS endpoint
+    # specified in the schema.
+    supports = []
+    for path in ga4gh.dos.schema.present_schema()['paths'].values():
+        for method in path.values():
+            supports.append(method['operationId'])
 
     def _make_request(self, meth, path, headers=None, body=None):
         """
@@ -133,28 +197,13 @@ class AbstractComplianceTest(unittest.TestCase):
         """
         return path + '?' + urllib.urlencode(kwargs)
 
+    @test_requires('ListDataObjects')
     def test_list_data_objects_simple(self):
         """Smoke test to verify that `GET /dataobjects` returns a response."""
         r = self.dos_request('GET', '/dataobjects')
         self.assertTrue(r)
 
-    # def test_list_data_objects_paging(self):
-    #     """Smoke test to see if data object paging works."""
-    #     page1 = self.dos_request('GET', '/ga4gh/dos/v1/dataobjects')
-    #     page2 = self.dos_request('GET', '/ga4gh/dos/v1/dataobjects?next_page_token=' + page1['next_page_token'])
-    #     self.assertNotEqual(page1['data_objects'][0], page2['data_object'][-1])
-
-    # def test_list_data_objects_pagesize(self):
-    #     """
-    #     Verify that `GET /dataobjects` handles the `page_size` parameter correctly.
-    #     """
-    #     page_size = 7
-    #     r = self.dos_request('GET', '/ga4gh/dos/v1/dataobjects?page_size=' + page_size)
-    #     self.assertEqual(len(r['data_objects']), page_size)
-
-    # def test_list_data_objects_filtering(self):
-    #     pass
-
+    @test_requires('ListDataObjects', 'GetDataObject')
     def test_get_data_object(self):
         """
         Lists Data Objects and then gets one by ID.
@@ -166,6 +215,7 @@ class AbstractComplianceTest(unittest.TestCase):
         data_object_2 = r['data_object']
         self.assertEqual(data_object_1, data_object_2)
 
+    @test_requires('ListDataBundles', 'GetDataBundle')
     def test_get_data_bundle(self):
         """
         Lists data bundles and then gets one by ID.
@@ -177,6 +227,7 @@ class AbstractComplianceTest(unittest.TestCase):
         data_bundle_2 = r['data_bundle']
         self.assertEqual(data_bundle_1, data_bundle_2)
 
+    @test_requires('GetDataBundle')
     def test_get_nonexistent_data_bundle(self):
         """
         Verifies that requesting a data bundle that doesn't exist results in HTTP 404
@@ -184,6 +235,7 @@ class AbstractComplianceTest(unittest.TestCase):
         self.dos_request('GET', '/databundles/NonexistentDataBundle',
                          expected_status=404)
 
+    @test_requires('UpdateDataObject')
     def test_update_nonexistent_data_object(self):
         """
         Verifies that trying to update a data object that doesn't exist
@@ -192,6 +244,7 @@ class AbstractComplianceTest(unittest.TestCase):
         self.dos_request(meth='PUT', expected_status=404,
                          path='/dataobjects/NonexistentObjID')
 
+    @test_requires('GetDataObject', 'ListDataObjects')
     def test_update_data_object_with_bad_request(self):
         """
         Verifies that attempting to update a data object with a malformed
@@ -201,6 +254,7 @@ class AbstractComplianceTest(unittest.TestCase):
         self.dos_request(meth='PUT', expected_status=400,
                          path='/dataobjects/' + data_obj['id'])
 
+    @test_requires('ListDataObjects')
     def test_paging(self):
         """
         Demonstrates basic paging features.
@@ -221,6 +275,7 @@ class AbstractComplianceTest(unittest.TestCase):
                                                        page_size=1, page_token=1))
         self.assertEqual(len(r['data_objects']), 1)
 
+    @test_requires('ListDataObjects')
     def test_nonexist_alias(self):
         """
         Test to ensure that looking up a nonexistent alias returns an
@@ -232,6 +287,7 @@ class AbstractComplianceTest(unittest.TestCase):
         body = self.dos_request('GET', self.get_query_url('/databundles', alias=alias))
         self.assertEqual(len(body['data_bundles']), 0)
 
+    @test_requires('ListDataObjects', 'UpdateDataObject', 'GetDataObject')
     def test_alias_update(self):
         """
         Demonstrates updating a data object with a given alias.
@@ -282,6 +338,7 @@ class AbstractComplianceTest(unittest.TestCase):
         # params['body']['data_object']['aliases'].remove(alias)
         # self.dos_request('PUT', url, **params)
 
+    @test_requires('ListDataObjects', 'UpdateDataObject')
     def test_full_data_object_update(self):
         """
         Demonstrates updating multiple fields of a data object at once.
