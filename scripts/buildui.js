3f934a37a1b35d7606e7f250a3d70d0ab03f156d
Brian O'Connor <briandoconnor@gmail.com>
2018-12-12 11:07:42 +0900

    transition to drs

--- scripts/buildui.js
+++ scripts/buildui.js
@@ -15,7 +15,7 @@ exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
 var SWAGGER_UI_DIST = path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
+//rm('-rf', 'web_deploy/swagger-ui/')
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 ls('web_deploy/swagger-ui')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
