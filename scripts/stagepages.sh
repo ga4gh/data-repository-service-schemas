f867c3502c28c3c5ac5efe2c3bb96bb365dd5541
Natan Lao <natanlao@users.noreply.github.com>
2019-01-21 10:59:11 -0800

    Fix bug where schema was incorrectly specified

--- scripts/stagepages.sh
+++ scripts/stagepages.sh
@@ -5,16 +5,16 @@ set -v
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     cp docs/html5/index.html docs/
-    cp openapi/workflow_execution_service.swagger.yaml ./swagger.yaml
+    cp openapi/data_repository_service_schemas.swagger.yaml ./swagger.yaml
     cp -R web_deploy/* .
 elif [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
-  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
-  branchpath="preview/$branch"
-  mkdir -p "$branchpath/docs"
-  cp docs/html5/index.html "$branchpath/docs/"
-  cp docs/pdf/index.pdf "$branchpath/docs/"
-  cp openapi/data_repository_service.swagger.yaml "$branchpath/swagger.yaml"
-  cp -R web_deploy/* "$branchpath/"
+    branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
+    branchpath="preview/$branch"
+    mkdir -p "$branchpath/docs"
+    cp docs/html5/index.html "$branchpath/docs/"
+    cp docs/pdf/index.pdf "$branchpath/docs/"
+    cp openapi/data_repository_service.swagger.yaml "$branchpath/swagger.yaml"
+    cp -R web_deploy/* "$branchpath/"
 fi
 
 # do some cleanup, these cause the gh-pages deploy to break
