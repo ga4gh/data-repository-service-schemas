4d42c75868b2a348bd1ca69e39dcb8adc4552cbb
Natan Lao <natanlao@users.noreply.github.com>
2019-01-22 07:11:01 -0800

    Reduce Swagger UI footprint
    This change should reduce repo footprint total by
    using a frame to produce the Swagger UI readout
    from the central, vendored copy of swagger-ui.

--- scripts/stagepages.sh
+++ scripts/stagepages.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-
-set -e
-set -v
+set -ev
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     cp docs/html5/index.html docs/
-    cp openapi/workflow_execution_service.swagger.yaml ./swagger.yaml
-    cp -R web_deploy/* .
+    cp openapi/data_repository_service.swagger.yaml ./swagger.yaml
+    mkdir -p swagger-ui/
+    cp docs/_swagger-ui-template.html swagger-ui/index.html
 elif [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
-  branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
-  branchpath="preview/$branch"
-  mkdir -p "$branchpath/docs"
-  cp docs/html5/index.html "$branchpath/docs/"
-  cp docs/pdf/index.pdf "$branchpath/docs/"
-  cp openapi/data_repository_service.swagger.yaml "$branchpath/swagger.yaml"
-  cp -R web_deploy/* "$branchpath/"
+    branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
+    branchpath="preview/$branch"
+    mkdir -p "$branchpath/docs"
+    cp docs/html5/index.html "$branchpath/docs/"
+    cp docs/pdf/index.pdf "$branchpath/docs/"
+    cp openapi/data_repository_service.swagger.yaml "$branchpath/swagger.yaml"
+    mkdir -p "$branchpath/swagger-ui/"
+    cp docs/_swagger-ui-template.html "$branchpath/swagger-ui/index.html"
 fi
 
 # do some cleanup, these cause the gh-pages deploy to break
