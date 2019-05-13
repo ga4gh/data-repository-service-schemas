3254b57e8ad35c14738fcd6345df99ad897a6c66
James Eddy <james.a.eddy@gmail.com>
2019-05-13 07:09:43 -0700

    Fix bug that causes non-master builds to erase master docs (#256)
    * modify 'fetchpages' script to preserve everything on current gh-pages
    
    * update travis config to keep old docs
    
    * clean up folders that aren't needed after gh-pages deploy
    
    * fix bash typo in stagepages script

--- scripts/fetchpages.sh
+++ scripts/fetchpages.sh
@@ -8,16 +8,6 @@ rm -rf .ghpages-tmp
 mkdir -p .ghpages-tmp
 cd .ghpages-tmp
 git clone --depth=1 --branch=gh-pages $REPO_URL .
-if [ "$TRAVIS_BRANCH" == "master" ]; then
 cp -Rn . ../
-else
-# in case it doesn't exist
-mkdir -p preview
-mkdir -p docs
-mkdir -p swagger-ui
-cp -Rn preview ../preview/
-cp -Rn docs ../docs/
-cp -Rn swagger-ui ../swagger-ui/
-fi
 cd ..
 rm -rf .ghpages-tmp
