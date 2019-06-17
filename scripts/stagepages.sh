eded33b44fdfdb6656db5b2221e5f2eff5ad08e4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-17 17:09:23 +0000

    Deploy ga4gh/data-repository-service-schemas to github.com/ga4gh/data-repository-service-schemas.git:gh-pages

--- scripts/stagepages.sh
+++ scripts/stagepages.sh
@@ -9,16 +9,17 @@ if [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
   else
     branch=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
     branchpath="preview/$branch"
-  fi  
+  fi
+  echo $branchpath
   mkdir -p "$branchpath/docs"
   cp docs/html5/index.html "$branchpath/docs/"
   cp docs/pdf/index.pdf "$branchpath/docs/"
   cp docs/asciidoc/*.png "$branchpath/docs/"
-  cp openapi/data_repository_service.swagger.yaml "$branchpath/swagger.yaml"
+  # cp openapi/data_repository_service.swagger.yaml "$branchpath/swagger.yaml"
   cp -R web_deploy/* "$branchpath/"
 fi
 
 # do some cleanup, these cause the gh-pages deploy to break
-rm -rf node_modules
-rm -rf web_deploy
-rm -rf spec
+# rm -rf node_modules
+# rm -rf web_deploy
+# rm -rf spec
